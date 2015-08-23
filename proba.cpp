cea92a999459e267579f9d39576b8cbf27ae1bd5
magdalena13 <magdalena.micic13@gmail.com>
2015-08-23 23:54:05 +0200

    Isprobavanje 2

--- proba.cpp
+++ proba.cpp
@@ -5,7 +5,12 @@ using namespace std;
 int main()
 {
     cout << "hello" << endl;
+
+    cout << "hello4" << endl;
+
     cout << "hello again" << endl;
+
+    cout << "hello3" << endl;
 
 
     return 0;
